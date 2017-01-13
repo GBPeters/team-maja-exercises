ce4ed57b1675255e92f54429be20fd26b030703a
Peters <gijs.peters@wur.nl>
2017-01-13 17:45:15 +0100

    (Pre)processing functions finished

--- R/lesson5.R
+++ R/lesson5.R
@@ -40,14 +40,14 @@ downloadData <- function(locations=DATA_LOCATIONS, datadir=DATA_DIR) {
 }
 
 ##
-#' @title createLandsaNDVItBrick
+#' @title createLandsatBrick
 #' @author Team Maja - Simon Veen & Gijs Peters
 #' @description Create raster brick from Landsat files
 #' @param suffix the start of the Landsat filenames to select files on
 #' @param datadir the data directory for searching data in
 #' @return a raster brick containing data from the landsat files found by the pattern
-#' @example createLandsatNDVIBrick("LC81970242014109")
-createLandsatNDVIBrick <- function(suffix, datadir = DATA_DIR) {
+#' @example createLandsatBrick("LC81970242014109")
+createLandsatBrick <- function(suffix, datadir = DATA_DIR) {
   bands = paste(BANDS, collapse="|")
   pattern <- paste0("^", suffix, ".*(", bands, ")\\.tif$")
   files <- list.files(datadir, pattern = pattern)
@@ -56,4 +56,59 @@ createLandsatNDVIBrick <- function(suffix, datadir = DATA_DIR) {
   rasters = lapply(files, raster)
   lbrick = brick(rasters)
   return (lbrick)
+}
+
+##
+#' @title cropRasterBricks
+#' @author Team Maja - Simon Veen & Gijs Peters
+#' @description Crops two raster bricks to their shared extent
+#' @param brick1 The first brick
+#' @param brick2 The second brick
+#' @param filename1 Filename for the first crop
+#' @param filename2 Filename for the second crop
+#' @return a vector with two bricks
+cropRasterBricks <- function(brick1, brick2, filename1="", filename2="") {
+  crop1 <- crop(brick1, brick2, filename=filename1, overwrite=TRUE)
+  crop2 <- crop(brick2, crop1, filename=filename2, overwrite=TRUE)
+  return(c(crop1, crop2))
+}
+
+##
+#' @title maskCloudsInBrick
+#' @author Team Maja - Simon Veen & Gijs Peters
+#' @description Uses one cloud layer to mask others in the raster brick
+#' @param lbrick The Landsat raster brick
+#' @param masklayer The number or name of the mask layer
+#' @param filename Filename for writing to disk
+#' @return a brick with the masked layers
+maskCloudsInBrick <- function(lbrick, masklayer=1, filename="") {
+  mask <- lbrick[[masklayer]]
+  maskedbrick = dropLayer(lbrick, masklayer)
+  maskedbrick <- overlay(maskedbrick, mask, fun=function(x, y) {x[y != 0] <- NA; return(x)}, filename=filename)
+  return(maskedbrick)
+}
+
+##
+#' @title calculateNDVI
+#' @author Team Maja - Simon Veen & Gijs Peters
+#' @description Calculate the NDVI based on a Red and NIR layer
+#' @param lbrick The raster brick to use for calculations
+#' @param red The layer name or number of the red band
+#' @param nir The layer name or number of the NIR band
+#' @return A raster layer with NDVI
+calculateNDVI <- function(lbrick, red, nir, filename="") {
+  ndvi <- calc(lbrick, fun=function(r) {(r[[nir]] - r[[red]]) / (r[[nir]] + r[[red]])}, filename=filename)
+  return(ndvi)
+}
+
+##
+#' @title calculateNDVIDiff
+#' @author Team Maja - Simon Veen & Gijs Peters
+#' @description Calculates the difference between NDVI in the first and the last layer of provided raster brick, assuming it is a time series. With more than two layers, the middle layers will be ignored.
+#' @param ndvibrick A raster brick containing layers with NDVI values.
+#' @param filename The file to store the resulting raster in
+#' @return A raster containing the NDVI difference.
+calculateNDVIDiff <- function(lbrick, filename="") {
+  diff <- calc(lbrick, fun=function(r) {r[[length(r)]]-r[[1]]}, filename=filename)
+  return(diff)
 }
--- R/main.R
+++ R/main.R
@@ -5,9 +5,22 @@
 source("./R/lesson5.R")
 
 # Download and extract data
-#downloadData()
+downloadData()
 
 # Create Bricks
-l8brick <- createLandsatNDVIBrick("LC81970242014109")
-l5brick <- createLandsatNDVIBrick("LT51980241990098")
-raster::plotRGB(l8brick)
+l8brick <- createLandsatBrick("LC81970242014109")
+l5brick <- createLandsatBrick("LT51980241990098")
+
+# Crop bricks
+crops <- cropRasterBricks(l5brick, l8brick, "l5crop.tif", "l8crop.tif")
+
+# Mask clouds
+masked <- lapply(crops, maskCloudsInBrick, masklayer=1)
+
+# Calculate NDVI
+ndvis <- lapply(masked, calculateNDVI, red=1, nir=2)
+
+# Calculate difference
+ndvis <- brick(ndvis)
+diff <- calculateNDVIDiff(ndvis)
+plot(diff)
