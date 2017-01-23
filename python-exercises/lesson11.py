1fb2913c8d467f2e1f4997c5ed0306a46b8a9396
GBPeters <gijstebrij@gmail.com>
2017-01-23 20:52:29 +0100

    Docstrings added to lesson11

--- python-exercises/lesson11.py
+++ python-exercises/lesson11.py
@@ -1,12 +1,12 @@
 # Imports
 from os import mkdir, path, system
-from osgeo.ogr import wkbPoint, Feature, GetDriverByName, Geometry
-from osgeo.osr import SpatialReference, CoordinateTransformation
 from random import sample
 from urllib import urlretrieve
 from zipfile import ZipFile
 
 import folium
+from osgeo.ogr import wkbPoint, Feature, GetDriverByName, Geometry
+from osgeo.osr import SpatialReference, CoordinateTransformation
 
 # Constants
 DATA_DIR = "../data"
@@ -15,6 +15,12 @@ SHP_DRIVER = GetDriverByName("ESRI Shapefile")
 
 
 def downloadData(url, datadir=DATA_DIR):
+    """
+    Download and extract a zip file
+    :param url: Url to download
+    :param datadir: Local directory to extract data to
+    :return: None
+    """
     try:
         mkdir(datadir)
     except:
@@ -26,11 +32,23 @@ def downloadData(url, datadir=DATA_DIR):
 
 
 def openShapeFile(filename=POINTS_FILE, datadir=DATA_DIR):
+    '''
+    Open an ESRI Shapefile
+    :param filename: The local filename to open
+    :param datadir: The directory where the file is stored
+    :return: The datasource object
+    '''
     p = path.join(datadir, filename)
     return SHP_DRIVER.Open(p)
 
 
 def selectRandomPoints(source, npoints):
+    """
+    Select a random sample of points from a given datasource
+    :param source: The Datasource to extract from
+    :param npoints: The number of points to sample.
+    :return: A list with (x, y) tuples extracted from the datasource
+    """
     layer = source.GetLayer()
     c = layer.GetFeatureCount()
     if npoints > c:
@@ -46,11 +64,24 @@ def selectRandomPoints(source, npoints):
     return xys
 
 
-def createWKTFromXY(lon, lat):
-    return "POINT (%f %f)" % (lon, lat)
+def createWKTFromXY(x, y):
+    """
+    Creates a WKT from specified x and y
+    :param x: The x-coordinate
+    :param y: The y-coordinate
+    :return: A WKT string
+    """
+    return "POINT (%f %f)" % (x, y)
 
 
 def transformGeometry(geom, source=4326, target=4326):
+    """
+    Transform a geometry.
+    :param geom: The geometry to transform
+    :param source: The source SRS EPSG code
+    :param target: The target SRS EPSG code
+    :return: The transformed geometry. Note, the original geometry is transformed, so technically this return is redundant.
+    """
     s = SpatialReference()
     t = SpatialReference()
     s.ImportFromEPSG(source)
@@ -60,11 +91,20 @@ def transformGeometry(geom, source=4326, target=4326):
     return geom
 
 
-def writePointsToShapefile(coords, filename, datadir=DATA_DIR, lyrname="lyr", crs=4326):
+def writePointsToShapefile(coords, filename, datadir=DATA_DIR, lyrname="lyr", epsg=4326):
+    """
+    Write a list of (x, y) tuples to an ESRI Shapefile
+    :param coords: The list of (x, y) tuples
+    :param filename: The name of target shapefile
+    :param datadir: The directory of target shapefile
+    :param lyrname: The name of the layer to store the points in
+    :param epsg: The EPSG code of the target layer
+    :return:
+    """
     fn = path.join(datadir, filename)
     ds = SHP_DRIVER.CreateDataSource(fn)
     srs = SpatialReference()
-    srs.ImportFromEPSG(crs)
+    srs.ImportFromEPSG(epsg)
     layer = ds.CreateLayer(lyrname, srs, wkbPoint)
     lyrdef = layer.GetLayerDefn()
     for x, y in coords:
@@ -78,6 +118,13 @@ def writePointsToShapefile(coords, filename, datadir=DATA_DIR, lyrname="lyr", cr
 
 
 def exportShapefiletoKML(infile, outfile, datadir=DATA_DIR):
+    """
+    Export an ESRI shapefile to a KML file
+    :param infile: The source shapefile name
+    :param outfile: The target KML file name
+    :param datadir: The directory for source an target files
+    :return: None
+    """
     inpath = path.join(datadir, infile)
     outpath = path.join(datadir, outfile)
     sh = "ogr2ogr -f KML %s %s" % (path.abspath(outpath), path.abspath(inpath))
@@ -85,6 +132,13 @@ def exportShapefiletoKML(infile, outfile, datadir=DATA_DIR):
 
 
 def exportShapefiletoGeoJSON(infile, outfile, datadir=DATA_DIR):
+    """
+    Export an ESRI shapefile to a GeoJSON file
+    :param infile: The source shapefile name
+    :param outfile: The target GeoJSON file name
+    :param datadir: The directory for source an target files
+    :return: None
+    """
     inpath = path.join(datadir, infile)
     outpath = path.join(datadir, outfile)
     sh = "ogr2ogr -f GeoJSON %s %s" % (path.abspath(outpath), path.abspath(inpath))
@@ -92,6 +146,12 @@ def exportShapefiletoGeoJSON(infile, outfile, datadir=DATA_DIR):
 
 
 def loadFoliumMap(filename, datadir=DATA_DIR):
+    """
+    Create a Folium/Leaflet map showing the contents of a GeoJSON file
+    :param filename: The GeoJSON file name
+    :param datadir: The directory containing the file
+    :return: A Folium Map object
+    """
     fn = path.join(datadir, filename)
     fmap = folium.Map(location=[52, 5.7], zoom_start=6)
     fmap.choropleth(geo_path=fn)
